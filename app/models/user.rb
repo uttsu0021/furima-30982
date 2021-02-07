aa460bb498202d8e278af5204921ca65c5805e38
uttsu0021 <uttsu0021@gmail.com>
2021-02-07 22:10:59 +0900

    with_optionsに変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,16 @@ class User < ApplicationRecord
   with_options presence: true do 
     validates :password, format: {with:/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'Include both letters and numbers'}
     validates :nickname
-    validates :last_name, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters'}
-    validates :first_name, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters'}
-    validates :last_name_katakana, format: {with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters'}
-    validates :first_name_katakana, format: {with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters'}
     validates :birthday
+  end
+
+  with_options presence: true, format: {with: /\A[ぁ-んァ-ン一-龥々]/, message: 'Full-width characters'} do
+    validates :last_name
+    validates :first_name
+  end
+
+  with_options presence: true,  format: {with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters'} do
+    validates :last_name_katakana
+    validates :first_name_katakana
   end
 end
