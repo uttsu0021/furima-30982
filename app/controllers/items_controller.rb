6fc4f74f04d957ad272aee3ddd8c8b5f7119a104
uttsu0021 <uttsu0021@gmail.com>
2021-02-09 06:30:01 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,8 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def item_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  end
 end
