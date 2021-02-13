cb95fd2137a56c477c0a1372714201aa1bc2f0f7
uttsu0021 <uttsu0021@gmail.com>
2021-02-13 16:46:39 +0900

    商品出品ページ表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,25 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only:[:new]
   def index
+    @items = Item.order("created_at DESC")
   end
 
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
   def item_params
-    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:product,:explanation,:category_id,:condition_id,:shipping_charge_id,:shipping_area_id,:days_to_ship_id, :price,:image).merge(user_id: current_user.id)
   end
 end
