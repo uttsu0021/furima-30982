82f4a7e6e55ab5e0b51be21d82139e14cf779d42
uttsu0021 <uttsu0021@gmail.com>
2021-02-16 15:01:30 +0900

    priceのテストコード追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -62,6 +62,16 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include('Price is not a number')
       end
+      it 'priceが半角英数混合では登録できないこと' do
+        @item.price = 'aa500'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price is not a number')
+      end
+      it 'priceが半角英語だけでは登録できないこと' do
+        @item.price = 'abcd'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price is not a number')
+      end
       it 'priceが300円未満では保存できないこと' do
         @item.price = 299
         @item.valid?
