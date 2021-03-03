ff2cb33317557ff50fe15ba03cca846d02005dbf
uttsu0021 <uttsu0021@gmail.com>
2021-03-03 22:57:04 +0900

    merge追加

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -7,7 +7,6 @@ class BuysController < ApplicationController
   def create
     @buy_address = BuyAddress.new(buy_address_params)
     @item = Item.find(params[:item_id])
-    
     if @buy_address.valid?
       @buy_address.save
       redirect_to root_path
@@ -18,7 +17,7 @@ class BuysController < ApplicationController
 
   private
   def buy_address_params
-    params.permit(:postal_code, :shipping_area_id, :municipality, :address, :building, :phone, :user_id, :item_id)
+    params.require(:buy_address).permit(:postal_code, :shipping_area_id, :municipality, :address, :building, :phone).merge(user_id: current_user.id,item_id: params[:item_id])
   end
 
 end
