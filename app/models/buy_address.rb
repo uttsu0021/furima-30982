2a5915ca507c447779627e3bea41be5b98732b6e
uttsu0021 <uttsu0021@gmail.com>
2021-03-03 22:57:53 +0900

    buy_id追加

--- app/models/buy_address.rb
+++ app/models/buy_address.rb
@@ -1,6 +1,6 @@
 class BuyAddress
    include ActiveModel::Model
-   attr_accessor :postal_code, :shipping_area_id, :municipality, :address, :building, :phone,:item_id,:user_id
+   attr_accessor :postal_code, :shipping_area_id, :municipality, :address, :building, :phone,:item_id,:user_id,:buy_id
    
    with_options presence: true do
      validates :postal_code, format: {with: /\A\d{3}[-]\d{4}\z/, message: "code can't be blank" }
@@ -12,7 +12,7 @@ class BuyAddress
    validates :shipping_area_id, numericality: { other_than: 1, message: "can't be blank" }
 
    def save
-    Buy.create(user_id: user_id, item_id: item_id)
-    Address.create(postal_code: postal_code, shipping_area_id: shipping_area_id, municipality: municipality, address: address, building: building, phone: phone)
+    buys = Buy.create(user_id: user_id, item_id: item_id)
+    Address.create(postal_code: postal_code, shipping_area_id: shipping_area_id, municipality: municipality, address: address, building: building, phone: phone, buy_id: buys.id)
    end
 end
